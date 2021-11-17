2d4ad7295d29f159ac98a2ccb23b8cc35fdb9d09
Valentin C <valentin.craciun99@gmail.com>
2021-11-17 10:14:52 +0200

    add util functions for decode data

--- native/service/cross_chain_manager/erd/proof.go
+++ native/service/cross_chain_manager/erd/proof.go
@@ -1,5 +1,7 @@
 package erd
 
+import "encoding/hex"
+
 const (
 	addressLen = 32
 )
@@ -11,4 +13,32 @@ type erdProof struct {
 	mainTrieRootHash []byte
 	key              []byte
 	address          []byte
+}
+
+func decodeProof(trieProofBytes [][]byte) [][]byte {
+	proof := make([][]byte, 0)
+	for _, hexProof := range trieProofBytes {
+		bytesProof, err := hex.DecodeString(string(hexProof))
+		if err != nil {
+			return nil
+		}
+
+		proof = append(proof, bytesProof)
+	}
+
+	return proof
+}
+
+func decodeAddress(address []byte) []byte {
+	bech32PubkeyConverter, err := NewBech32PubkeyConverter(addressLen)
+	if err != nil {
+		return nil
+	}
+
+	decodedAddress, err := bech32PubkeyConverter.Decode(string(address))
+	if err != nil {
+		return nil
+	}
+
+	return decodedAddress
 }
